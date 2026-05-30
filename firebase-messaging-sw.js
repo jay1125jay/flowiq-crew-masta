0e986e1ac3ee8cd0d375b4deb5252c09b160eec8
FlowIQCEO <kimaro201020@gmail.com>
2026-05-31 00:35:51 +0900

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+importScripts("https://www.gstatic.com/firebasejs/12.14.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.14.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD4gNqGPWf5e7YSPUMpXfI5sRq70pLPH-M",
+  authDomain: "wexa-push.firebaseapp.com",
+  projectId: "wexa-push",
+  storageBucket: "wexa-push.firebasestorage.app",
+  messagingSenderId: "119368741125",
+  appId: "1:119368741125:web:1b6d9fe5c8ac0783cf876c",
+  measurementId: "G-WNMDWD2XC4"
+});
+
+firebase.messaging();
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then(function(clientList) {
+      for (const client of clientList) {
+        if ("focus" in client) return client.focus();
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow("/flowiq-crew-masta/master.html");
+      }
+    })
+  );
+});
